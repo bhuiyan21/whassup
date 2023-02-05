37c94276e9f7a020b3ba266fa3e5e82c85d58168
bhuiyan21 <eliasbhuiyan21@gmail.com>
2023-02-05 20:21:24 +0600

    test

--- src/component/Friend.js
+++ src/component/Friend.js
@@ -88,7 +88,7 @@ const Friend = () => {
                 <FcSearch className='text-2xl cursor-pointer text-secondar'/>
             </div>
         </div>
-           <div className='h-[650px] overflow-y-scroll pr-6'>
+           <div className='h-[650px] overflow-y-scroll pr-6 flex flex-wrap gap-4'>
             {
               accept.length == 0
               ? <p className='h-full flex items-center justify-center font-medium font-poppins text-2xl text-shadow'>Add users in your friend list </p>
@@ -99,14 +99,14 @@ const Friend = () => {
                 <div>
                   {
                     delateBox && dltGroup.includes(item.receiverId + item.senderId)
-                    ?<div className='p-7 w-full text-center'>
-                    <p className='font-nunito font-bold text-xl text-primary p-6'>Are you sure you want to block your Friend <span className='text-secondary'>{item.receiverName}</span> ?</p>
+                    ?<div className='w-44 flex justify-center items-center text-center h-64'>
+                    <p className='font-nunito font-semibold text-lg text-primary p-6'>Are you sure you want to block your Friend <span className='text-secondary'>{item.receiverName}</span> ?</p>
                     <button onClick={()=>handelBlock(item)} className='inline-block py-2 px-5 mr-3 bg-secondary font-semibold font-poppins text-sm text-white rounded-lg'>Yes</button>
                     <button onClick={()=>setDelateBox(!delateBox)} className='inline-block py-2 px-5 bg-secondary font-semibold font-poppins text-sm text-white rounded-lg'>No</button>
                   </div>
-                  : <div className='flex py-4 border-b-2'>
-                  <div className='mr-4 w-[52px] h-[54px]'>
-                     <img className='w-full h-full rounded-full' src={item.receiverprofile}/>
+                  : <div className='py-4 border-b-2 w-44 shadow-lg mb-4'>
+                  <div className='mr-4'>
+                     <img className='w-full h-full rounded-md' src={item.receiverprofile}/>
                   </div>
                   <div>
                       <h2 className='font-semibold font-poppins text-sm mt-2'>{item.receiverName}</h2>
@@ -117,29 +117,35 @@ const Friend = () => {
                  onClick={()=>handelDelateBox(item)}>
                   Block
                  </button>
+                  <button className='inline-block py-2 px-2 bg-secondary font-semibold font-poppins text-sm text-white rounded-lg'>
+                  Unfriend
+                 </button>
                   </div>
                 </div>
                   }
                 </div>
                 : <div>
                   { delateBox && dltGroup.includes(item.senderId + item.receiverId)
-                    ?<div className='p-7 w-full text-center'>
-                    <p className='font-nunito font-bold text-xl text-primary p-6'>Are you sure you want to block your Friend <span className='text-secondary'>{item.senderName}</span> ?</p>
+                    ?<div className='w-44 flex justify-center items-center text-center h-64'>
+                    <p className='font-nunito font-semibold text-lg text-primary p-6'>Are you sure you want to block your Friend <span className='text-secondary'>{item.senderName}</span> ?</p>
                     <button onClick={()=>handelBlock(item)} className='inline-block py-2 px-5 mr-3 bg-secondary font-semibold font-poppins text-sm text-white rounded-lg'>Yes</button>
                     <button onClick={()=>setDelateBox(!delateBox)} className='inline-block py-2 px-5 bg-secondary font-semibold font-poppins text-sm text-white rounded-lg'>No</button>
                   </div>
-                  :<div className='flex py-4 border-b-2'>
-                  <div className='mr-4 w-[52px] h-[54px]'>
-                    <img className='w-full h-full rounded-full' src={item.senderprofile}/>
+                  :<div className='py-4 border-b-2 w-44 shadow-lg mb-4'>
+                  <div className='mr-4'>
+                    <img className='w-full h-full rounded-md' src={item.senderprofile}/>
                   </div>
                       <div>
-                        <h2 className='font-semibold font-poppins text-sm mt-2'>{item.senderName}</h2>
+                        <h2 className='font-semibold font-poppins text-sm mt-2'>dsfgd{item.senderName}</h2>
                         <p className='font-medium font-poppins text-xs text-shadow'>{item.senderemail}</p>
                       </div>
-                      <div className='mt-2 ml-auto'>
+                      <div className='mt-2 ml-auto flex gap-2'>
                       <button className='inline-block py-2 px-2 bg-secondary font-semibold font-poppins text-sm text-white rounded-lg'
                        onClick={()=>handelDelateBox(item)} >
                         Block
+                      </button>
+                      <button className='inline-block py-2 px-2 bg-secondary font-semibold font-poppins text-sm text-white rounded-lg'>
+                        Unfriend
                       </button>
                     </div>
                   </div>
@@ -153,23 +159,26 @@ const Friend = () => {
                     <div>
                       {
                         delateBox && dltGroup.includes(item.receiverId + item.senderId)
-                        ?<div className='p-7 w-full text-center'>
-                        <p className='font-nunito font-bold text-xl text-primary p-6'>Are you sure you want to block your Friend <span className='text-secondary'>{item.receiverName}</span> ?</p>
+                        ?<div className='w-44 flex justify-center items-center text-center h-64'>
+                        <p className='font-nunito font-semibold text-lg text-primary p-6'>Are you sure you want to block your Friend <span className='text-secondary'>{item.receiverName}</span> ?</p>
                         <button onClick={()=>handelBlock(item)} className='inline-block py-2 px-5 mr-3 bg-secondary font-semibold font-poppins text-sm text-white rounded-lg'>Yes</button>
                         <button onClick={()=>setDelateBox(!delateBox)} className='inline-block py-2 px-5 bg-secondary font-semibold font-poppins text-sm text-white rounded-lg'>No</button>
                       </div>
-                      : <div className='flex py-4 border-b-2'>
-                      <div className='mr-4 w-[52px] h-[54px]'>
-                         <img className='w-full h-full rounded-full' src={item.receiverprofile}/>
+                      : <div className='py-4 border-b-2 w-44 shadow-lg mb-4'>
+                      <div className='mr-4'>
+                         <img className='w-full h-full rounded-md' src={item.receiverprofile}/>
                       </div>
                       <div>
                           <h2 className='font-semibold font-poppins text-sm mt-2'>{item.receiverName}</h2>
                           <p className='font-medium font-poppins text-xs text-shadow'>{item.receiveremail}</p>
                       </div>
-                      <div className='mt-2 ml-auto'>
+                      <div className='mt-2 ml-auto flex gap-2'>
                       <button className='inline-block py-2 px-2 bg-secondary font-semibold font-poppins text-sm text-white rounded-lg'
-                     onClick={()=>handelDelateBox(item)}>
+                      onClick={()=>handelDelateBox(item)}>
                       Block
+                     </button>
+                      <button className='inline-block py-2 px-2 bg-secondary font-semibold font-poppins text-sm text-white rounded-lg'>
+                      Unfriend
                      </button>
                       </div>
                     </div>
@@ -177,23 +186,29 @@ const Friend = () => {
                     </div>
                     : <div>
                       { delateBox && dltGroup.includes(item.senderId + item.receiverId)
-                        ?<div className='p-7 w-full text-center'>
-                        <p className='font-nunito font-bold text-xl text-primary p-6'>Are you sure you want to block your Friend <span className='text-secondary'>{item.senderName}</span> ?</p>
+                        ?<div className='w-44 flex justify-center items-center text-center h-64'>
+                          <div>
+                        <p className='font-nunito font-semibold text-md text-primary p-6'>Are you sure you want to block your Friend <span className='text-secondary'>{item.senderName}</span> ?</p>
                         <button onClick={()=>handelBlock(item)} className='inline-block py-2 px-5 mr-3 bg-secondary font-semibold font-poppins text-sm text-white rounded-lg'>Yes</button>
                         <button onClick={()=>setDelateBox(!delateBox)} className='inline-block py-2 px-5 bg-secondary font-semibold font-poppins text-sm text-white rounded-lg'>No</button>
+                        </div>
                       </div>
-                      :<div className='flex py-4 border-b-2'>
-                      <div className='mr-4 w-[52px] h-[54px]'>
-                        <img className='w-full h-full rounded-full' src={item.senderprofile}/>
+                      :<div className='py-4 border-b-2 w-44 shadow-lg mb-4'>
+                      <div className='mr-4'>
+                        <img className='w-full h-full rounded-md' src={item.senderprofile}/>
                       </div>
                           <div>
                             <h2 className='font-semibold font-poppins text-sm mt-2'>{item.senderName}</h2>
                             <p className='font-medium font-poppins text-xs text-shadow'>{item.senderemail}</p>
                           </div>
-                          <div className='mt-2 ml-auto'>
+                          <div className='mt-2 ml-auto flex gap-2'>
                           <button className='inline-block py-2 px-2 bg-secondary font-semibold font-poppins text-sm text-white rounded-lg'
                            onClick={()=>handelDelateBox(item)} >
                             Block
+                          </button>
+                          <button className='inline-block py-2 px-2 bg-secondary font-semibold font-poppins text-sm text-white rounded-lg'
+                            >
+                            Unfriend
                           </button>
                         </div>
                       </div>
--- src/pages/friends/index.js
+++ src/pages/friends/index.js
@@ -3,19 +3,40 @@ import Block from '../../component/Block'
 import Friendreq from '../../component/Friendreq'
 import Friend from '../../component/Friend'
 import Sidebar from '../../component/Sidebar'
+import { AiOutlineUserAdd} from 'react-icons/ai';
+import { FaUserFriends} from 'react-icons/fa';
+import { ImBlocked} from 'react-icons/im';
 const Friends = () => {
+    let handeluserlist =()=>{
+        
+    }
   return (
-    <div>
-        <Sidebar/>
-        <div className='mx-24 flex justify-between'>
-            <div className='w-[45%]'>
-             <Friend/>
-            </div>
-            <div className='w-[45%]'>
-              <Friendreq/>
-              <Block/>
-            </div>
+    <div> 
+        <div className='absolute top-0 left-0 w-full'>
+          <Sidebar/>
         </div>
+        <div className='pt-44 flex h-screen justify-between'>
+            <div className='pl-3 font-openSans font-semibold text-shadow text-2xl w-1/6 shadow-[10px_0px_3px_-7px_rgb(0,0,0,0.2)]'>
+                <div onClick={handeluserlist} className='flex items-center gap-2 cursor-pointer hover:bg-slate-100 py-2 pl-3 mr-3 rounded-lg'>
+                    <AiOutlineUserAdd/>
+                    <h2>User list</h2>
+                </div>
+                <div className='flex items-center gap-2 cursor-pointer hover:bg-slate-100 py-2 pl-3 mr-3 rounded-lg'>
+                    <FaUserFriends/>
+                    <h2>Friend list</h2>
+                </div>
+                <div className='flex items-center gap-2 cursor-pointer hover:bg-slate-100 py-2 pl-3 mr-3 rounded-lg'>
+                    <ImBlocked/>
+                    <h2>Block list</h2>
+                </div>
+            </div>
+            <div className=' w-1/2'>
+              <Friend/>
+            </div>
+            <div className='w-3/12'>
+              <Friend/>
+            </div>
+         </div>
     </div>
   )
 }
